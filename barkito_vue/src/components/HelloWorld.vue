5d554101721d92147ee38c89e2c5d73b7407a031
Daniel <danieleduardocald@gmail.com>
2022-06-08 00:04:59 -0400

    Change on submit to on click

--- barkito_vue/src/components/HelloWorld.vue
+++ barkito_vue/src/components/HelloWorld.vue
@@ -16,15 +16,19 @@ export default {
   },
   methods: {
     async handleUserAuthentication() {
+      
+      const user = {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      }
+      
       try {
-        const req = await axios.post('http://localhost:3000/api/v1/users', {
-          username: this.username,
-          email: this.email,
-          password: this.password,
-      })
+        const req = await axios.post('http://localhost:3000/api/v1/users', user)
         } catch (error) {
           console.log(error);
     }
+    console.log(user)
   }
 },
 };
@@ -40,7 +44,7 @@ export default {
           <Input class="mbe10" v-model="this.username" id="username" placeholder="Enter your username" label="Username" type="text" />
           <Input class="mbe10" v-model="this.email" id="email" placeholder="Enter your email" label="Email" type="text" />
           <Input class="mbe10" v-model="this.password" id="password" placeholder="Enter your password" label="Password" type="text" />
-          <Button isRounded isBlock @submit="handleUserAuthentication()">Login</Button>
+          <Button isRounded isBlock @click="handleUserAuthentication()">Login</Button>    
       </section> 
       </div>
     </Card>
