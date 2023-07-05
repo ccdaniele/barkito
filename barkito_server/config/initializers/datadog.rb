e03b4afb45add63a2073a2702234c556786a920a
ccdaniele <danieleduardocald@gmail.com>
2023-07-05 13:32:31 -0400

    sqlite

--- barkito_server/config/initializers/datadog.rb
+++ barkito_server/config/initializers/datadog.rb
@@ -1,4 +1,14 @@
+require 'ddtrace'
+
 Datadog.configure do |c|
-    # Add additional configuration here.
-    # Activate integrations, change tracer settings, etc...
-  end
+  c.tracing.instrument :rails
+  c.service = 'randrest'
+  c.env = 'dev'
+
+  c.tracing.report_hostname = true
+  # c.tracing.test_mode.enabled = (ENV['RACK_ENV'] == 'dev')
+  c.tracing.log_injection = true
+  c.runtime_metrics.enabled = true
+  # c.runtime_metrics.statsd = Datadog::Statsd.new
+  c.diagnostics.startup_logs.enabled = false
+end
--- server-barkito/app/models/user_vendor.rb
+++
@@ -1,7 +0,0 @@
-class UserVendor < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :user
-    belongs_to :vendor
-=======
->>>>>>> 6d013a78 (next.js migration)
-end
